89aa4edc1bba0c6f69cde891d571b6a0e548fcef
zyad <zyadbassem9090@gmail.com>
2024-07-13 21:40:20 +0300

    adding front for sign

--- commerce/onlineshop/models.py
+++ commerce/onlineshop/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    username = models.CharField(max_length=32)
+    hashPassword = models.CharField(max_length=64)
--- commerce/onlineshop/views.py
+++ commerce/onlineshop/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import user
 # Create your views here.
+
+def sign(request):
+    return render(request, "sign.html")
